28973f858b98741460375fa8654eb7d9dcc04638
Ryan-Willpower <surasak.c.work@gmail.com>
2020-01-23 21:06:39 +0700

    fix: change test case

--- __tests__/insert.ts
+++ __tests__/insert.ts
@@ -3,11 +3,11 @@ import Ryan from "../lib/"
 describe("`INSERT` query", () => {
   it("Should build `INSERT` query correctly", () => {
     const query = Ryan.insertInto(`table`, ["userid", "username"])
-      .value("xxxx", "ryan")
+      .values("xxxx", "ryan")
       .buildQuery()
 
     expect(query).toBe(
-      "INSERT INTO TABLE `table` (`userid`, `username`) VALUE (`xxxx`, `ryan`)"
+      "INSERT INTO TABLE `table` (`userid`, `username`) VALUES (`xxxx`, `ryan`)"
     )
   })
 })
